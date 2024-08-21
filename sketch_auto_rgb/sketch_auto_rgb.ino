62aabaa0f509ed4cdce8072a2b3c12f5d9dbd27d
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-08-21 10:02:31 -0400

    I have made the code probably work
    I need to test the code next, But it looks correct enough

--- sketch_auto_rgb/sketch_auto_rgb.ino
+++ sketch_auto_rgb/sketch_auto_rgb.ino
@@ -3,6 +3,10 @@ const int photoRes = A0; // Photoresistor at Arduino analog pin A0
 
 // Variables
 int value; // Store value from photoresistor (0-1023)
+int redValue;
+int greenValue;
+int blueValue;
+int arr_len = 9;
 
 // Define Pins
 #define BLUE1 2
@@ -15,100 +19,92 @@ int value; // Store value from photoresistor (0-1023)
 #define GREEN3 9
 #define RED3 10
 
+int leds[] = {BLUE1,GREEN1,RED1,BLUE2,GREEN2,RED2,BLUE3,GREEN3,RED3};
+
 void setup(){
- pinMode(ledPin, OUTPUT); // Set lepPin - 9 pin as an output
- pinMode(photoRes, INPUT); // Set photoRes - A0 pin as an input
+  pinMode(photoRes, INPUT); // Set photoRes - A0 pin as an input
+  for( int i = 0; i < arr_len; ++i ){
+    pinMode(leds[i], OUTPUT);
+    digitalWrite(led[i], LOW);
+  }
 }
 
 void loop(){
+  #define delayTime 10 // fading time between colors
+  if(checkPhotoRes() == true){
+    redValue = 255; // choose a value between 1 and 255 to change the color.
+    greenValue = 0;
+    blueValue = 0;
+
+    for(int i = 0; i < 255; i += 1) // fades out red bring green full when i=255
+    {
+      if(checkPhotoRes() == false){
+        break;
+      }
+      redValue -= 1;
+      greenValue += 1;
+      analogWrite(RED1, redValue);
+      analogWrite(GREEN1, greenValue);
+      analogWrite(RED2, redValue);
+      analogWrite(GREEN2, greenValue);
+      analogWrite(RED3, redValue);
+      analogWrite(GREEN3, greenValue);
+      delay(delayTime);
+    }
+
+    redValue = 0;
+    greenValue = 255;
+    blueValue = 0;
+
+    for(int i = 0; i < 255; i += 1) // fades out green bring blue full when i=255
+    {
+      if(checkPhotoRes() == false){
+        break;
+      }
+      greenValue -= 1;
+      blueValue += 1;
+      analogWrite(GREEN1, greenValue);
+      analogWrite(BLUE1, blueValue);
+      analogWrite(GREEN2, greenValue);
+      analogWrite(BLUE2, blueValue);
+      analogWrite(GREEN3, greenValue);
+      analogWrite(BLUE3, blueValue);
+      delay(delayTime);
+    }
+
+    redValue = 0;
+    greenValue = 0;
+    blueValue = 255;
+
+    for(int i = 0; i < 255; i += 1) // fades out blue bring red full when i=255
+    {
+      if(checkPhotoRes() == false){
+        break;
+      }
+      blueValue -= 1;
+      redValue += 1;
+      analogWrite(BLUE1, blueValue);
+      analogWrite(RED1, redValue);
+      analogWrite(BLUE2, blueValue);
+      analogWrite(RED2, redValue);
+      analogWrite(BLUE3, blueValue);
+      analogWrite(RED3, redValue);
+      delay(delayTime);
+    }
+  }
+}
+
+bool checkPhotoRes(){
   value = analogRead(photoRes);
   
   // You can change value "25"
   if (value > 25){
-    digitalWrite(ledPin, LOW); // Turn led off
+    return true
   }
   else{
-    digitalWrite(ledPin, HIGH); // Turn led on
+    return false
   }
 
   delay(500); // Small delay
 }
 
-
-// RGB code from Elegoo
-
-
-// void setup()
-// {
-// pinMode(RED, OUTPUT);
-// pinMode(GREEN, OUTPUT);
-// pinMode(BLUE, OUTPUT);
-// digitalWrite(RED, HIGH);
-// digitalWrite(GREEN, LOW);
-// digitalWrite(BLUE, LOW);
-// }
-
-// // define variables
-// int redValue;
-// int greenValue;
-// int blueValue;
-
-// // main loop
-// void loop()
-// {
-// #define delayTime 10 // fading time between colors
-
-// redValue = 255; // choose a value between 1 and 255 to change the color.
-// greenValue = 0;
-// blueValue = 0;
-
-// // this is unnecessary as we've either turned on RED in SETUP
-// // or in the previous loop ... regardless, this turns RED off
-// // analogWrite(RED, 0);
-// // delay(1000);
-
-// for(int i = 0; i < 255; i += 1) // fades out red bring green full when i=255
-// {
-// redValue -= 1;
-// greenValue += 1;
-// // The following was reversed, counting in the wrong directions
-// // analogWrite(RED, 255 - redValue);
-// // analogWrite(GREEN, 255 - greenValue);
-// analogWrite(RED, redValue);
-// analogWrite(GREEN, greenValue);
-// delay(delayTime);
-// }
-
-// redValue = 0;
-// greenValue = 255;
-// blueValue = 0;
-
-// for(int i = 0; i < 255; i += 1) // fades out green bring blue full when i=255
-// {
-// greenValue -= 1;
-// blueValue += 1;
-// // The following was reversed, counting in the wrong directions
-// // analogWrite(GREEN, 255 - greenValue);
-// // analogWrite(BLUE, 255 - blueValue);
-// analogWrite(GREEN, greenValue);
-// analogWrite(BLUE, blueValue);
-// delay(delayTime);
-// }
-
-// redValue = 0;
-// greenValue = 0;
-// blueValue = 255;
-
-// for(int i = 0; i < 255; i += 1) // fades out blue bring red full when i=255
-// {
-// // The following code has been rearranged to match the other two similar sections
-// blueValue -= 1;
-// redValue += 1;
-// // The following was reversed, counting in the wrong directions
-// // analogWrite(BLUE, 255 - blueValue);
-// // analogWrite(RED, 255 - redValue);
-// analogWrite(BLUE, blueValue);
-// analogWrite(RED, redValue);
-// delay(delayTime);
-// }
-// }
